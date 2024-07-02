41f2c9afdb8cefb8709c2f738a32fa60fd82c077
Jordan-Prescott <Jprescott23@gmail.com>
2024-07-02 18:28:36 +0100

    supabase configured

---
+++ app/dependancies/supabase.py
@@ -0,0 +1,10 @@
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
+
+data = supabase.table("characters").select("*").execute()
+
--- app/main.py
+++ app/main.py
@@ -5,4 +5,15 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    
+    import os
+    from supabase import create_client, Client
+
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_KEY")
+
+    supabase: Client = create_client(url, key)
+
+    data = supabase.table("characters").select("*").execute()
+    
+    return data
