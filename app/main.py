c872b029d170fbfd505887079353d1bacfc49640
Jordan-Prescott <Jprescott23@gmail.com>
2024-07-03 13:52:33 +0100

    Home page added
    FastAPI now serves home page with dobby club

--- app/main.py
+++ app/main.py
@@ -1,19 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 
 @app.get("/")
-async def root():
-    
-    import os
-    from supabase import create_client, Client
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-    url: str = os.environ.get("SUPABASE_URL")
-    key: str = os.environ.get("SUPABASE_KEY")
 
-    supabase: Client = create_client(url, key)
-
-    data = supabase.table("characters").select("*").execute()
-    
-    return data
+# @app.get("/")
+# async def root(): 
+#     import os
+#     from supabase import create_client, Client
+#     url: str = os.environ.get("SUPABASE_URL")
+#     key: str = os.environ.get("SUPABASE_KEY")
+#     supabase: Client = create_client(url, key)
+#     data = supabase.table("characters").select("*").execute()    
+#     return data
