db14a866e4202cec1f5b3a4f01553283c3036d05
Jordan-Prescott <Jprescott23@gmail.com>
2024-09-12 14:39:19 +0100

    script complete

--- app/schemas/script.py
+++ app/schemas/script.py
@@ -1,8 +1,19 @@
 from pydantic import BaseModel
+from enum import Enum
 
+class URLChoices(Enum):
+    SERIES_1 = '1'
+    SERIES_2 = '2'
+    SERIES_3 = '3'
+    SERIES_4 = '4'
+    SERIES_5 = '5'
+    SERIES_6 = '6'
+    SERIES_7 = '7'
+    SERIES_8 = '8'
+    SERIES_9 = '9'
 
 class Script(BaseModel):
-    script_content: str
-    writers: list[str]
     series: int
     episode: int
+    writers: str
+    script_content: str
