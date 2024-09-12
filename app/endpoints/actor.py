5c365f1218090fda556f54a0a3ca9b631bced9c8
Jordan-Prescott <Jprescott23@gmail.com>
2024-09-12 13:33:33 +0100

    Series and Actors completed

--- app/endpoints/actor.py
+++ app/endpoints/actor.py
@@ -1,7 +1,35 @@
 from fastapi import APIRouter, HTTPException
+from typing import List, Optional
 
-router = APIRouter(prefix="/actors", tags=["actor"])
+from ..db.supabase import SupabaseClient
+from ..schemas.actor import Actor
 
-@router.get("/")
-async def get_actors():
-    return {"actor": "actor endpoint"}
+router = APIRouter(prefix="/actors", tags=["actors"])
+
+db = SupabaseClient().get_client()
+
+@router.get("/", response_model=List[Actor])
+async def get_actors(
+    first_name: Optional[str] = None, 
+    last_name: Optional[str] = None
+    ) -> List[Actor]:
+    
+    query = db.table("actor").select(
+        "first_name, last_name, gender, participation_range, total_episodes"
+    )
+
+    if first_name:
+        first_name = first_name.capitalize()
+        query = query.eq("first_name", first_name)
+    if last_name:
+        last_name = last_name.capitalize()
+        query = query.eq("last_name", last_name)
+
+    response = query.execute()
+    
+    if not response:
+        raise HTTPException(status_code=404, detail="Actors not found")
+
+    return [
+        Actor(**actor) for actor in response.data
+    ]
--- app/endpoints/series.py
+++ app/endpoints/series.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+from typing import List, Optional
 
 from ..db.supabase import SupabaseClient
 from ..schemas.series import Series, SeriesURLChoices
@@ -8,21 +9,22 @@ router = APIRouter(prefix="/series", tags=["series"])
 db = SupabaseClient().get_client()
 
 @router.get("/")
-async def get_series(series_num: SeriesURLChoices | None = None) -> list[Series]:
+async def get_series(
+    series_number: Optional[SeriesURLChoices] | None = None
+    ) -> List[Series]:
     
-    if series_num:
-        response = db.table("series").select(
-            "title, description, network, premier_date, finale_date, number_of_episodes"
-            ).eq("title", f"Series {series_num.value}").execute().dict()
-        if not response:
-            raise HTTPException(status_code=404, detail="Series not found")
-        return [
-            Series(**series) for series in response["data"]
-        ]
-    
-    response = db.table("series").select(
+    query = db.table("series").select(
         "title, description, network, premier_date, finale_date, number_of_episodes"
-        ).execute().dict()
+    )
+    
+    if series_number:
+       query = query.eq("title", f"Series {series_number.value}")
+    
+    response = query.execute()
+    
+    if not response:
+        raise HTTPException(status_code=404, detail="Series not found")
+    
     return [
-        Series(**series) for series in response["data"]
+        Series(**series) for series in response.data
     ]
